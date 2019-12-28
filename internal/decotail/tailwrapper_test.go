ac458b441b23035cb23b7333ebf95ce6dfd7a092
kuritayu <summergirlna@gmail.com>
2019-12-29 00:57:52 +0900

    improve expected

--- internal/decotail/tailwrapper_test.go
+++ internal/decotail/tailwrapper_test.go
@@ -1,16 +1,21 @@
 package decotail
 
 import (
-	"fmt"
+	"github.com/aybabtme/color/brush"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestConvertToColorOK(t *testing.T) {
 	tw := New("/var/log/system.log", true, "AAAA")
-	fmt.Println(tw.convertToColor("AAAA"))
+	actual := tw.convertToColor("AAAA")
+	expected := brush.Red("AAAA").String()
+	assert.Equal(t, expected, actual)
 }
 
 func TestConvertToColorNG(t *testing.T) {
 	tw := New("/var/log/system.log", true, "AAAA")
-	fmt.Println(tw.convertToColor("AAAB"))
+	actual := tw.convertToColor("AAAB")
+	expected := "AAAB"
+	assert.Equal(t, expected, actual)
 }
