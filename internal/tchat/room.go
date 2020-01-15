9cbbd4f4cc32f50292f9ebb49a3a1c0fc5284b5f
kuritayu <summergirlna@gmail.com>
2020-01-15 16:11:17 +0900

    add comment

--- internal/tchat/room.go
+++ internal/tchat/room.go
@@ -4,14 +4,17 @@ type room struct {
 	clients []*Client
 }
 
+// NewRoomはルームを設定する。
 func NewRoom() *room {
 	return &room{}
 }
 
+// Addはルームにクライアントを追加する。
 func (r *room) Add(c *Client) {
 	r.clients = append(r.clients, c)
 }
 
+// Sendはルームにいるクライアント全員にメッセージを送信する。
 func (r *room) Send(ch <-chan []byte) {
 	msg := <-ch
 	for _, cl := range r.clients {
